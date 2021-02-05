e78d9b518b9dbb0788bc6c28a25288359b86bca2
Debarshi Bhaumik <debarshi@compucorp.co.uk>
2021-02-05 22:46:48 +0530

    test

--- index.js
+++ index.js
@@ -1,23 +1,5 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const { Octokit } = require("@octokit/core");
+const run = require('run');
 
-(async () => {
-  await new Promise();
-  try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    // console.log(`The event payload: ${payload}`);
+run();
 
-    var pulls = await Octokit.request('GET /deb1990/uk.co.compucorp.civicase/pulls');
 
-    console.log(pulls)
-  } catch (error) {
-    core.setFailed(error.message);
-  }
-})();
