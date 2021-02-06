074042d28c16c12c62e207e25f6cc9d49088a0a6
Debarshi Bhaumik <debarshi@compucorp.co.uk>
2021-02-06 12:10:58 +0530

    test

--- dist/index.js
+++ dist/index.js
@@ -5808,12 +5808,6 @@ async function run () {
 
     console.log(pulls)
 
-
-
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
-
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     console.log(`The event payload: ${payload}`);
