7ef29f9bbb698681a8d1970d732c1d1be81693d8
Debarshi Bhaumik <debarshi@compucorp.co.uk>
2021-03-02 23:34:36 +0530

    Create Release PR Creator Github Action

--- src/create-pr.js
+++ src/create-pr.js
@@ -213,7 +213,7 @@ async function getAllMergeCommitsSinceBeginning () {
 async function getPullRequests (lastRelease) {
   let commits;
 
-  lastRelease.tag_name = '1.11.0';
+  var lastRelease = { tag_name: '1.11.0' };
   if (lastRelease) {
     commits = await getMergeCommitsSince(lastRelease.tag_name);
   } else {
